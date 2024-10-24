c2b5c1b57be8250e5503470926dcc190cab9897d
Surabhi <surabhisadanand92@gmail.com>
2024-10-23 23:23:01 -0400

    Update create-vm.ps1

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,3 +1,19 @@
-$cred = Get-Credential
-New-AzVm -ResourceGroupName "myResourceGroup" -Name "myVM" -Location "CanadaCentral" -Image "Canonical:UbuntuServer:18.04-LTS:18.04.202401161" -Credential $cred
+# Connect to Azure account using a managed identity (used in Automation Accounts or Azure VMs)
 
+# Authenticate using Managed Identity
+$null = Connect-AzAccount -Identity
+
+# Define variables for the resource group creation
+$resourceGroupName = "MyRG"
+$location = "EastUS"
+
+# Check if the resource group exists
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+
+if (-not $resourceGroup) {
+    # Create the resource group if it doesn't exist
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+    Write-Output "Resource group '$resourceGroupName' created in location '$location'."
+} else {
+    Write-Output "Resource group '$resourceGroupName' already exists."
+}
